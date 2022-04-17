55b9afc7ffa7ce31154b852a2edd4fd94e66380b
Anđela Popović <andjela9popovic@gmail.com>
2022-04-18 01:48:05 +0200

    prvi zahtev ispunjen :)

---
+++ GameShop/GameShop/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameShop
+{
+    class Product
+    {
+        private string Name { get; set; }
+        private int UPC { get; set; }
+        private double price { get; set; }
+
+        public Product(string name, int upc, double price)
+        {
+            this.Name = name;
+            this.UPC = upc;
+            this.price = price;
+        }
+
+        public double fixedTax(double price) {
+            return Math.Round(0.8 * price, 2);
+        }
+
+        public double customTax(double price, double tax)
+        {
+            return Math.Round(((100 - tax) / 100) * price, 2);
+        }
+
+        public string Ispis(double price, double tax)
+        {
+            return $"{Name}, UPC = {UPC}, cena = {price}.\n " +
+                $"Cena {price} pre poreza i {this.fixedTax(price)} nakon 20% poreza. \n " +
+                $"Cena {price} pre poreza i {this.customTax(price, tax)} nakon {tax}% poreza";
+        }
+
+
+    }
+}
--- GameShop/GameShop/Program.cs
+++ GameShop/GameShop/Program.cs
@@ -6,8 +6,8 @@ namespace GameShop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Zdravo");
+            Product product1 = new Product("LEGO blokovi = Friends Forest House", 41679, 20.25);
+            Console.WriteLine(product1.Ispis(52.34, 26));
         }
     }
 }
