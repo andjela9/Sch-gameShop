a73318a2040c58329ed302b59537848068cdf129
Anđela Popović <andjela9popovic@gmail.com>
2022-04-18 02:07:20 +0200

    dodato da korisnik moze preko konzole da unese procenat popusta

--- GameShop/GameShop/Program.cs
+++ GameShop/GameShop/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace GameShop
 {
@@ -7,7 +8,17 @@ namespace GameShop
         static void Main(string[] args)
         {
             Product product1 = new Product("LEGO blokovi = Friends Forest House", 41679, 20.25);
-            Console.WriteLine(product1.Ispis(52.34, 26));
+            Console.WriteLine("Unesite zeljeni procenat poreza: ");
+            string sTax = Console.ReadLine();
+            if(!(Double.TryParse(sTax, out double tax)))
+            {
+                Console.WriteLine("Pogresan unos. Procenat mora biti u formatu broja.");
+            }
+            else
+            {
+                Console.WriteLine(product1.Ispis(52.34, tax));
+            }
+            
         }
     }
 }
