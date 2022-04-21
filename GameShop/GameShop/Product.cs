5abcced79b1d4dacba3eef50bd99f22325623b46
Anđela Popović <andjela9popovic@gmail.com>
2022-04-21 14:40:20 +0200

    zavrsen prvi task

--- GameShop/GameShop/Product.cs
+++ GameShop/GameShop/Product.cs
@@ -9,6 +9,7 @@ namespace GameShop
         private string Name { get; set; }
         private int UPC { get; set; }
         private double price { get; set; }
+        
 
         public Product(string name, int upc, double price)
         {
@@ -17,30 +18,31 @@ namespace GameShop
             this.price = price;
         }
 
-        public double fixedTax(double price) {
-            return Math.Round(0.8 * price, 2);
+        public double fixedTax() {
+            return Math.Round(0.8 * this.price, 2);
         }
 
-        public double customTax(double price, double tax)
+        public double customTax(double tax)
         {
-            return Math.Round(((100 - tax) / 100) * price, 2);
+            return Math.Round(((100 - tax) / 100) * this.price, 2);
         }
 
-        public string Ispis(double price, double tax)
+        public string Ispis(double tax)
         {
-            return $"{Name}, UPC = {UPC}, cena = {price}.\n " +
-                $"Cena {price} pre poreza i {this.fixedTax(price)} nakon 20% poreza. \n " +
-                $"Cena {price} pre poreza i {this.customTax(price, tax)} nakon {tax}% poreza";
+            return $"\n\nIme = {Name}, UPC = {UPC}, cena = {price}.\n " +
+                //$"Cena {price} pre poreza i {this.fixedTax()} nakon 20% poreza. \n " +
+                $"Cena {price} pre poreza i {this.customTax(tax)} nakon {tax}% poreza\n\n";
         }
         //      dodati unos naziva, upc, cene, zastite za to (validacija)
         //      upc mora biti celobrojan, bez specijalnih karaktera itd
         //      naziv ne sme prazan i da nema razmak
         //      decimala da prihvata i . i , i za cenu i za procenat
         //      ogranicen br karaktera po polju, npr 64
-        // ne mora da ispise posle 20% nego zadatog
+        //      ne mora da ispise posle 20% nego zadatog
         //      exit na dugme
         //      da moze da racuna sa 20% (unet na kraju %)
         //      negativan broj
+        //      DONE
 
 
     }
--- GameShop/GameShop/Program.cs
+++ GameShop/GameShop/Program.cs
@@ -9,14 +9,15 @@ namespace GameShop
         {
             ValidateInput validator1 = new ValidateInput();
             Product product1 = new Product("LEGO blokovi = Friends Forest House", 41679, 20.25);
+            string sName, name, sTax, sUpc; 
            
             while (true)
             {
                 
                 Console.WriteLine("Unesite naziv proizvoda. Za izlazak unesite exit");
-                string sName = Console.ReadLine();
+                sName = Console.ReadLine();
                 if (sName == "exit") break;
-                string name = "-1";
+                name = "-1";
                 if (validator1.ValidName(sName) != "-1")
                 {
                     name = validator1.ValidName(sName);
@@ -25,11 +26,11 @@ namespace GameShop
                 {
                     Console.WriteLine("Pogresen unos. Naziv ne sme biti prazan.");
                 }
-                Console.WriteLine($"{name}\n");
+                //Console.WriteLine($"{name}\n");
 
 
                 Console.WriteLine("Unesite zeljeni procenat poreza: Za izlazak unesite exit");
-                string sTax = Console.ReadLine();
+                sTax = Console.ReadLine();
                 if (sTax == "exit") break;
                 double tax = -1;
                 if (validator1.ValidTax(sTax) != -1)
@@ -40,11 +41,11 @@ namespace GameShop
                 {
                     Console.WriteLine("Pogresan unos. Porez mora biti pozitivan broj do 64 karaktera");
                 }
-                Console.WriteLine($"{tax}\n");
+                //Console.WriteLine($"{tax}\n");
 
 
                 Console.WriteLine("Unesite upc: Za izlazak unesite exit");
-                string sUpc = Console.ReadLine();
+                sUpc = Console.ReadLine();
                 if (sUpc == "exit") break;
                 int upc = -1;
                 if (validator1.ValidUPC(sUpc) != -1)
@@ -55,13 +56,13 @@ namespace GameShop
                 {
                     Console.WriteLine("Pogresan unos. UPC mora biti pozitivan celobrojan broj do 64 karaktera");
                 }
-                Console.WriteLine($"{upc}\n");
+                //Console.WriteLine($"{upc}\n");
 
 
                 Console.WriteLine("Unesite cenu: Za izlazak unesite exit");
                 string sPrice = Console.ReadLine();
                 if (sPrice == "exit") break;
-                double price = 0;
+                double price = -1;
                 if (validator1.ValidatePrice(sPrice) != -1)
                 {
                     price = validator1.ValidatePrice(sPrice);
@@ -70,8 +71,19 @@ namespace GameShop
                 {
                     Console.WriteLine("Pogresan unos. Cena mora biti pozitivan broj do 64 karaktera");
                 }
-                Console.WriteLine($"{price}\n");
+                //Console.WriteLine($"{price}\n");
+                if (name != "-1" && tax != -1 && upc != -1 && price != -1)
+                {
+                    Product product = new Product(name, upc, price);
+                    product.customTax(tax);
+                    Console.WriteLine(product.Ispis(tax));
+                }
+                else
+                {
+                    Console.WriteLine("U unosu postoji greska!");
+                }
             }
+         
 
         }
     }
